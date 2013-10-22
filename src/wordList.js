e82264b818c1cecb1125d4798ec0addb49d53853
davmillar <dave@davegoesthedistance.com>
2013-10-22 16:23:14 -0400

    Fixed a bug where the randomly-picked word length was being generated between 4 and 13 instead of 4 and 10, optimized the letter selection to use ASCII character codes instead of a hard-coded alphabet, and changed the looping mechanism to do/while to keep code DRY. Also renamed minified files to keep with common practices.

--- src/wordList.js
+++ src/wordList.js
@@ -1,10 +1,10 @@
-/*
- *Word List JS
- *Purpose: This module create the Word_List variable, which handles word-checking and random word generation.
- *Instructions: Add <script> element to include this file first, and then another <script> element to add the wordBank file.
- *Author: Tim Crouch
- *Date: 10.9.2012
- *Version: 0.1
+/* 
+ * Word List JS
+ * Purpose: This module creates the Word_List variable, which handles word-checking and random word generation.
+ * Instructions: Add <script> element to include this file first, and then another <script> element to add the wordBank file.
+ * Author: Tim Crouch
+ * Date: 10.9.2012
+ * Version: 0.21
  */
 
 var Word_List = (function() {
@@ -13,15 +13,12 @@ var Word_List = (function() {
     
     //Create method for checking if word is in list
     function listCheck(toCheck) {
+    
         toCheck = toCheck.toLowerCase();
         var letter = toCheck.charAt(0);
         
         //wordBank will be an array initialized with the full word list, indexed by letter.
-        if (wordBank[letter].indexOf(toCheck) > -1) {
-            return true;
-        } else {
-            return false;
-        }
+        return (wordBank[letter].indexOf(toCheck) > -1);
         
     }
     
@@ -31,7 +28,7 @@ var Word_List = (function() {
         
         if (letterCount == 0) {
             //If 0 was entered, get a random number of digits between 4 and 10
-            letterCount = Math.floor(Math.random() * 10) + 4;
+            letterCount = Math.floor(Math.random() * 7) + 4;
         }
         
         //Check bounds
@@ -44,27 +41,19 @@ var Word_List = (function() {
         }
         
         //Randomize the letter you are looking in
-        var checkLetterNum = Math.floor(Math.random() * 26) + 1;
-        
-        //Create a letter array
-        var allLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-        
-        var checkLetter = allLetters[checkLetterNum - 1];
-        
-        var wordNum;
-        
-        //Randomly choose the number of an element in the correct wordBank letter array
-        wordNum = Math.floor(Math.random() * wordBank[checkLetter].length);        
-        var wordPick = wordBank[checkLetter][wordNum - 1];
+        var checkLetterNum = Math.floor(Math.random() * 26),
+        		checkLetter = String.fromCharCode(checkLetterNum + 97),
+        		wordNum,
+        		wordPick;
         
         //check if the word is the correct length and repeat if not
-        while (wordPick.length != letterCount) {
+        do {
+		        //Randomly choose the number of an element in the correct wordBank letter array
             wordNum = Math.floor(Math.random() * wordBank[checkLetter].length);        
-        var wordPick = wordBank[checkLetter][wordNum - 1];
-        }
+      			wordPick = wordBank[checkLetter][wordNum - 1];
+        } while (wordPick.length != letterCount)
         
         return wordPick;     
-       
     }
     
     function loadWordDat(wordArray) {
